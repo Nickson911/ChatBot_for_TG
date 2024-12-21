ea1055a8bf52ccb8bff109425a4cbf34130fd7ca
Neto test <denis.157911@mail.ru>
2024-12-21 15:18:46 +0400

    Initial commit

---
+++ src/main/java/ru/netology/chatbot_for_tg/ChatBotForTgApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.chatbot_for_tg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotForTgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotForTgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/chatbot_for_tg/ChatBotForTgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.chatbot_for_tg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotForTgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
